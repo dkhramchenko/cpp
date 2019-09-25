8c692e81857f82ecb3556d3158fa8504297819f7
dkhramchenko <d.khramchenko@mail.ru>
2019-09-25 14:58:49 +0300

    lab1 task1 done

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+        static void Task1()
+        {
+            Console.WriteLine("sdfdsfdsfdsfs"); // вывели текст при стандартных настройках
+            Console.ForegroundColor = ConsoleColor.DarkRed; // сменили цвет текста на красный
+            Console.WriteLine("sdasdasda");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta; // сменили цвет фона на розовый
+            Console.WriteLine("sdasdas");
+            Console.ResetColor(); // восстановили настройки поумолчанию
+            Console.WriteLine("asdasdas");
+            Console.ReadLine();
+        }
+    }
+}
