76ba6a502929439a2cf2c0a24e4299d10e2c7eb7
dkhramchenko <d.khramchenko@mail.ru>
2019-09-25 16:51:14 +0300

    task5

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DeepPink;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile(@"C:\test.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3();
+            f.Show();
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+            textBox1.Clear();
+            textBox1.BackColor = Color.Azure;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+        }
+    }
+}
