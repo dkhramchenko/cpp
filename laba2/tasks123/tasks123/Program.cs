5ffe38af216fae634ff05a9f3005af1ab4e7e494
dkhramchenko <d.khramchenko@mail.ru>
2019-09-26 18:56:49 +0300

    task1 lab2 done

---
+++ laba2/tasks123/tasks123/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Console.ReadKey();
+        }
+        static void Task1()
+        {
+            // Тип DateTime
+            System.DateTime dat1 = new System.DateTime(2019, 09, 11, 10, 45, 22); // создание переменной с помощью конструктора класса DateTime
+            System.DateTime dat2 = Convert.ToDateTime("07.03.2018 10:35:12"); // создание переменной с помощью конвертации строки
+            Console.WriteLine("Прошло = {0} дней, часов, минут и секунд", dat1 - dat2);
+        }
+    }
+}
