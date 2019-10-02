c19c16e299bd6e06541832ab6bff09883d92c9ba
dkhramchenko <d.khramchenko@mail.ru>
2019-10-03 01:31:34 +0300

    lab3 task2

---
+++ laba3/task2/task2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "red",
+            "blue",
+            "grey",
+            "green",
+            "black",
+            "yellow"});
+            this.listBox1.Location = new System.Drawing.Point(52, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 154);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(173, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 95);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EXAMPLE FOR\r\nSTUDY";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Упражнения";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ laba3/task2/task2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string c1, c2;
+            c1 = listBox1.SelectedItem.ToString();
+            if (c1 == "red")
+            {
+                button1.BackColor = Color.FromName(c1);
+                c2 = "gray";
+                button1.ForeColor = Color.FromName(c2);
+            }
+            if (c1 == "blue")
+            {
+                button1.BackColor = Color.FromName(c1);
+                c2 = "pink";
+                button1.ForeColor = Color.FromName(c2);
+            }
+            if (c1 == "yellow")
+            {
+                button1.BackColor = Color.FromName(c1);
+                c2 = "green";
+                button1.ForeColor = Color.FromName(c2);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string color1, color2;
+            color1 = listBox1.SelectedItem.ToString();
+            label1.ForeColor = Color.FromName(color1);
+            if (color1 == "red")
+            {
+                color2 = "gray";
+                label1.BackColor = Color.FromName(color2);
+            }
+            else if (color1 == "blue")
+            {
+                color2 = "yellow";
+                label1.BackColor = Color.FromName(color2);
+            }
+            else
+                label1.BackColor = Color.FromName("pink");
+        }
+    }
+}
