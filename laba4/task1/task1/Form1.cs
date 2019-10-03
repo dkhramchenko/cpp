b8e50d0651a57f76a72248b36e17e7604aeca7f6
dkhramchenko <d.khramchenko@mail.ru>
2019-10-04 01:44:53 +0300

    lab4 task1 done

---
+++ laba4/task1/task1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontStyle fs = new FontStyle();
+            string f = checkedListBox1.SelectedItem.ToString();
+            switch (f)
+            {
+                case "Bold":
+                    fs = FontStyle.Bold;
+                    break;
+                case "Italic":
+                    fs = FontStyle.Italic;
+                    break;
+                case "Underline":
+                    fs = FontStyle.Underline;
+                    break;
+            }
+            Font font = new Font("Arial", 18, fs);
+            label1.Font = font;
+        }
+    }
+}
