ac4175d6322f5edcf9f68f26dc673ebe01571aee
dkhramchenko <d.khramchenko@mail.ru>
2019-10-03 16:53:35 +0300

    lab3 task3 done

--- laba3/task3/task3/Form1.Designer.cs
+++ laba3/task3/task3/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -84,13 +86,34 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(336, 117);
             this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Пересчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Завершить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -112,6 +135,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- laba3/task3/task3/Form1.cs
+++ laba3/task3/task3/Form1.cs
@@ -16,5 +16,25 @@ namespace task3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("Данные неполные!");
+            }
+            else
+            {
+                double priceUSD = Convert.ToDouble(textBox1.Text);
+                double rate = Convert.ToDouble(textBox2.Text);
+                double rub = priceUSD * rate;
+                label4.Text = textBox1.Text + " USD = " + rub.ToString() + "р.";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
