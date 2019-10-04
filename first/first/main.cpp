ace46fb796baed85da39f3e2e7120d904f90903a
dkhramchenko <d.khramchenko@mail.ru>
2019-10-04 13:29:54 +0300

    Update main.cpp

--- first/first/main.cpp
+++ first/first/main.cpp
@@ -10,30 +10,6 @@ using namespace std;
 
 int main()
 {
-	cout << 1 * 1 << " ";
-	cout << 2 * 2 << " ";
-	cout << 3 * 3 << " ";
-	cout << 4 * 4 << " ";
-	cout << 5 * 5 << " ";
-
-	cout << endl;
-
-	int i = 1;
-	while (i <= 5)
-	{
-		cout << i * i << " ";
-		++i;
-	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 	sequence::task20();
-=======
-	cout << endl;
-
->>>>>>> parent of 08695f4... dz
-=======
-	cout << endl;
-
->>>>>>> parent of 08695f4... dz
 	return 0;
 }
