61a0041d45b64fa0e05dc65b3ed7b59dbadc6de9
dkchramchenko <d.kchramchenko@mail.ru>
2019-10-07 01:55:23 +0300

    lab5 task3 done

---
+++ laba5/task3/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int x = r.Next(1, 7);
+            Console.WriteLine("Бросание кубика: " + x);
+            while (x != 6)
+            {
+                x = r.Next(1, 7);
+                Console.WriteLine("Бросание кубика: " + x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
