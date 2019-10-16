5e60babc1f498c25d5b0cd6b33ed61814b9d5551
dkhramchenko <d.khramchenko@mail.ru>
2019-10-16 15:26:12 +0300

    lab task3 fixed

--- laba3/task3/task3/Form1.cs
+++ laba3/task3/task3/Form1.cs
@@ -19,27 +19,53 @@ namespace task3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "" || textBox2.Text == "")
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+            if (t1 == "" || t2 == "")
             {
                 System.Windows.Forms.MessageBox.Show("Данные неполные!");
+                return;
             }
-            else
+            int zapyatayat1 = 0;
+            foreach (var symbol in t1)
             {
-                double priceUSD = Convert.ToDouble(textBox1.Text);
-                double rate = Convert.ToDouble(textBox2.Text);
-                double rub = priceUSD * rate;
-                label4.Text = String.Format("{0:f2} USD = {1:f2}р.", priceUSD, rub);
+                if (symbol == ',')
+                {
+                    ++zapyatayat1;
+                }
+                if (char.IsDigit(symbol) != true && symbol != ',')
+                {
+                    System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                    return;
+                }
             }
+            int zapyatayat2 = 0;
+            foreach (var symbol in t2)
+            {
+                if (symbol == ',')
+                {
+                    ++zapyatayat2;
+                }
+                if (char.IsDigit(symbol) != true && symbol != ',')
+                {
+                    System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                    return;
+                }
+            }
+            if (zapyatayat1 > 1 || zapyatayat2 > 1)
+            {
+                System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                return;
+            }
+            double priceUSD = Convert.ToDouble(t1);
+            double rate = Convert.ToDouble(t2);
+            double rub = priceUSD * rate;
+            label4.Text = String.Format("{0:f2} USD = {1:f2}р.", priceUSD, rub);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
-        }
-
-        private void TextBox1_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
