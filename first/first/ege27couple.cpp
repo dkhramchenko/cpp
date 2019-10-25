003122ed9de8e3f883b587e89f175d5870210e02
Qvocha <pineappletoe@gmail.com>
2019-10-25 15:28:40 +0300

    lesson end

--- first/first/ege27couple.cpp
+++ first/first/ege27couple.cpp
@@ -51,4 +51,115 @@ namespace ege27couple
 		}
 		cout << sum - min;
 	}
+
+	int sumOfDigit(int n)
+	{
+		int s = 0;
+
+		while (n != 0)
+		{
+			s += n % 10;
+			n /= 10;
+		}
+
+		return s;
+	}
+
+	void task13423()
+	{
+		int N = 0;
+		int number = 0;
+		int max = INT_MIN;
+		int maxInd = 0;
+		cin >> N;
+		int* a = new int[28];
+		for (int i = 0; i < 28; i++)
+		{
+			a[i] = 0;
+		}
+		for (int i = 0; i < N; i++)
+		{
+			cin >> number;
+			int sum = sumOfDigit(number);
+			++a[sum];
+		}
+		for (int i = 0; i < 28; i++)
+		{
+			if (a[i] > max)
+			{
+				max = a[i];
+				maxInd = i;
+			}
+		}
+		cout << maxInd;
+		delete[] a;
+	}
+
+	void task13476()
+	{
+		int N = 0;
+		cin >> N;
+		int number = 0;
+		int min = INT_MAX;
+		int minInd = 0;
+		int* a = new int[28];
+		for (int i = 0; i < 28; i++)
+		{
+			a[i]=0;
+		}
+		for (int i = 0; i < N; i++)
+		{
+			cin >> number;
+			int sum = sumOfDigit(number);
+			++a[sum];
+		}
+		for (int i = 0; i < 28; i++)
+		{
+			if (a[i] <= min)
+			{
+				min = a[i];
+				minInd = i;
+			}
+		}
+		cout << minInd;
+		delete[] a;
+	}
+
+	void task13503()
+	{
+		int N = 0;
+		cin >> N;
+		int number = 0;
+		int max = INT_MIN;
+		int* a = new int[10];
+		for (int i = 0; i < 10; i++)
+		{
+			a[i] = 0;
+		}
+		for (int i = 0; i < N; i++)
+		{
+			cin >> number;
+			while (number != 0)
+			{
+				int digit = number % 10;
+				++a[digit];
+				number /= 10;
+			}
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			if (a[i] > max)
+			{
+				max = a[i];
+			}
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			if (a[i] == max)
+			{
+				cout << i;
+			}
+		}
+		delete[] a;
+	}
 }
--- first/first/ege27couple.h
+++ first/first/ege27couple.h
@@ -3,4 +3,10 @@
 namespace ege27couple
 {
 	void task11363();
+
+	void task13423();
+
+	void task13476();
+
+	void task13503();
 }
