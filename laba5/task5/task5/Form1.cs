a2c7f3d2f5538429830a0ed251680f4def7c1dca
Qvocha <pineappletoe@gmail.com>
2019-11-07 11:50:48 +0300

    lab5 task5 checked

--- laba5/task5/task5/Form1.cs
+++ laba5/task5/task5/Form1.cs
@@ -19,34 +19,64 @@ namespace task5
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "" || textBox2.Text == "")
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+            if (t1 == "" || t2 == "")
             {
                 System.Windows.Forms.MessageBox.Show("Введите данные!");
             }
+            int zapyatayat1 = 0;
+            foreach (var symbol in t1)
+            {
+                if (symbol == ',')
+                {
+                    ++zapyatayat1;
+                }
+                if (char.IsDigit(symbol) != true && symbol != ',')
+                {
+                    System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                    return;
+                }
+            }
+            int zapyatayat2 = 0;
+            foreach (var symbol in t2)
+            {
+                if (symbol == ',')
+                {
+                    ++zapyatayat2;
+                }
+                if (char.IsDigit(symbol) != true && symbol != ',')
+                {
+                    System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                    return;
+                }
+            }
+            if (zapyatayat1 > 1 || zapyatayat2 > 1)
+            {
+                System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                return;
+            }
+            int width = Convert.ToInt32(textBox1.Text);
+            int height = Convert.ToInt32(textBox2.Text);
+            int k;
+            string s;
+            if (radioButton1.Checked)
+            {
+                k = 1;
+                s = "Однокамерный";
+            }
             else
             {
-                int width = Convert.ToInt32(textBox1.Text);
-                int height = Convert.ToInt32(textBox2.Text);
-                int k = 0;
-                string s = "";
-                if (radioButton1.Checked)
-                {
-                    k = 1;
-                    s = "Однокамерный";
-                }
-                else
-                {
-                    k = 2;
-                    s = "Двухкамерный";
-                }
-                int pod = 0;
-                if (checkBox1.Checked)
-                {
-                    pod = 1000;
-                }
-                int price = width * height * k * 10 + pod;
-                label5.Text = string.Format("Размер окна: {0} х {1} см\nСтеклопакет: {2}\nСтоимость: {3}р.", width, height, s, price);
+                k = 2;
+                s = "Двухкамерный";
             }
+            int pod = 0;
+            if (checkBox1.Checked)
+            {
+                pod = 1000;
+            }
+            int price = width * height * k * 10 + pod;
+            label5.Text = string.Format("Размер окна: {0} х {1} см\nСтеклопакет: {2}\nСтоимость: {3}р.", width, height, s, price);
         }
 
         private void Form1_Load(object sender, EventArgs e)
