43f56874545615fc604c7a4809f2c4103ff5b1d5
Qvocha <pineappletoe@gmail.com>
2019-11-07 11:39:07 +0300

    lab5 task4 checked

--- laba5/task4/task4/Form1.cs
+++ laba5/task4/task4/Form1.cs
@@ -21,33 +21,63 @@ namespace task4
         {
             string s1 = textBox1.Text;
             string s2 = textBox2.Text;
-            
+
             if (s1 == "" || s2 == "")
             {
-                System.Windows.Forms.MessageBox.Show("Введите данные!");
+                System.Windows.Forms.MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            int zapyatayat1 = 0;
+            foreach (var symbol in s1)
+            {
+                if (symbol == ',')
+                {
+                    ++zapyatayat1;
+                }
+                if (char.IsDigit(symbol) != true && symbol != ',')
+                {
+                    System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                    return;
+                }
+            }
+            int zapyatayat2 = 0;
+            foreach (var symbol in s2)
+            {
+                if (symbol == ',')
+                {
+                    ++zapyatayat2;
+                }
+                if (char.IsDigit(symbol) != true && symbol != ',')
+                {
+                    System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                    return;
+                }
+            }
+            if (zapyatayat1 > 1 || zapyatayat2 > 1)
+            {
+                System.Windows.Forms.MessageBox.Show("Данные некорректные!");
+                return;
+            }
+
+            int width = Convert.ToInt32(s1);
+            int height = Convert.ToInt32(s2);
+            int alum = 20;
+            int plast = 10;
+            int price = width * height;
+            string mat = "";
+
+            if (radioButton1.Checked)
+            {
+                price *= alum;
+                mat = "алюминий";
             }
             else
             {
-                int width = Convert.ToInt32(s1);
-                int height = Convert.ToInt32(s2);
-                int alum = 20;
-                int plast = 10;
-                int price = width * height;
-                string mat = "";
-
-                if (radioButton1.Checked)
-                {
-                    price *= alum;
-                    mat = "алюминий";
-                }
-                else
-                {
-                    price *= plast;
-                    mat = "пластик";
-                }
-
-                label3.Text = String.Format("Размер: {0:f2} x {1:f2} см\nМатериал: {2}\nСтоимость: {3:f2}р.", width, height, mat, price);
+                price *= plast;
+                mat = "пластик";
             }
+
+            label3.Text = String.Format("Размер: {0:f2} x {1:f2} см\nМатериал: {2}\nСтоимость: {3:f2}р.", width, height, mat, price);
         }
 
         private void Form1_Load(object sender, EventArgs e)
