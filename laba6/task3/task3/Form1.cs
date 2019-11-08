338bdc8e5dd3185dd73c40039b33e9cab4452a16
Qvocha <pineappletoe@gmail.com>
2019-11-08 14:50:11 +0300

    laba6 task3 done

--- laba6/task3/task3/Form1.cs
+++ laba6/task3/task3/Form1.cs
@@ -16,5 +16,21 @@ namespace task3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[][] m = new int[3][];
+            m[0] = new int[] { 1, 2 };
+            m[1] = new int[] { 3, 4, 5, 6 };
+            m[2] = new int[] { 7, 8, 9 };
+            for (int row = 0; row < m.Length; ++row)
+            {
+                for (int col = 0; col < m[row].Length; ++col)
+                {
+                    richTextBox1.Text += String.Format("{0}{1} элемент = {2};", row, col, m[row][col]);
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
     }
 }
