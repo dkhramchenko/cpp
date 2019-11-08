bbaeeabfb51b7fdc6fbf0c421c9204c90afffdfe
Qvocha <pineappletoe@gmail.com>
2019-11-08 14:40:57 +0300

    comment laba6 task2 done

--- laba6/task2/task2/Form1.cs
+++ laba6/task2/task2/Form1.cs
@@ -17,16 +17,16 @@ namespace task2
             InitializeComponent();
         }
 
-        int[,] m = new int[,] { { 4, 9, 12 }, { 56, 45, 23 }, { 3, 67, 89 } };
+        int[,] m = new int[,] { { 4, 9, 12 }, { 56, 45, 23 }, { 3, 67, 89 } }; // объявление и инициализация двумерного массива m
         private void button1_Click(object sender, EventArgs e)
         {   
             for (int i = 0; i <= m.GetUpperBound(0); ++i)
             {
                 for (int j = 0; j <= m.GetUpperBound(1); ++j)
                 {
-                    richTextBox1.Text += String.Format("{0}{1} элемент = {2};", i, j, m[i, j]);
+                    richTextBox1.Text += String.Format("{0}{1} элемент = {2};", i, j, m[i, j]); // добавление элемента массива(строка i, столбец j, элемент m[i, j]) в richtextbox1
                 }
-                richTextBox1.Text += "\n";
+                richTextBox1.Text += "\n"; // переход на новую строчку после добавления всех элементов текущей(i-ой) строки массива m
             }
         }
     }
