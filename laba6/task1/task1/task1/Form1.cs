732d4c1ca7ddbf4cc4332c7a9703f8db9a5fac73
Qvocha <pineappletoe@gmail.com>
2019-11-08 14:17:30 +0300

    laba6 task1 done

---
+++ laba6/task1/task1/task1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 4, 9, 12, 56, 45 };
+            foreach (var n in numbers)
+            {
+                int index = Array.IndexOf(numbers, n);
+                string s = String.Format("{0} элемент = {1}", index, n);
+                listBox1.Items.Add(s);
+            }
+        }
+    }
+}
