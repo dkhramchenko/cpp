2d710986a958d1e0400462d629d86e178089a9b4
Qvocha <pineappletoe@gmail.com>
2019-11-14 18:04:37 +0300

    laba6 task7 done

--- laba6/task7/task7/Form1.Designer.cs
+++ laba6/task7/task7/Form1.Designer.cs
@@ -30,36 +30,112 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(32, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(120, 65);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "А..Я";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(181, 40);
+            this.button2.Location = new System.Drawing.Point(216, 40);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(107, 65);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Я..А";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Иванов Р.Д.",
+            "Петров Н.А.",
+            "Сидоров В.В.",
+            "Абрамов А.А."});
+            this.listBox1.Location = new System.Drawing.Point(32, 131);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(291, 279);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(366, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(366, 224);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 59);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(91, 442);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 56);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(544, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Редактировать строку";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(521, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 31);
+            this.textBox1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(835, 537);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +143,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- laba6/task7/task7/Form1.cs
+++ laba6/task7/task7/Form1.cs
@@ -16,5 +16,87 @@ namespace task7
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Список пуст!");
+                return;
+            }
+            textBox1.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Список пуст!");
+                return;
+            }
+            listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Список пуст!");
+                return;
+            }
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            if (listBox1.Items.Count != 0)
+            {
+                listBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Список пуст!");
+                return;
+            }
+            List<string> l = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; ++i)
+            {
+                l.Add(listBox1.Items[i].ToString());
+            }
+            l.Sort();
+            listBox1.Items.Clear();
+            for (int i = 0; i < l.Count; ++i)
+            {
+                listBox1.Items.Add(l[i]);
+            }
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Список пуст!");
+                return;
+            }
+            List<string> l = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; ++i)
+            {
+                l.Add(listBox1.Items[i].ToString());
+            }
+            l.Sort();
+            l.Reverse();
+            listBox1.Items.Clear();
+            for (int i = 0; i < l.Count; ++i)
+            {
+                listBox1.Items.Add(l[i]);
+            }
+            listBox1.SelectedIndex = 0;
+        }
     }
 }
