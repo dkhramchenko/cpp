f25f4b6296703eb0e818554dcf9484873ecb243c
Qvocha <pineappletoe@gmail.com>
2019-11-21 19:32:56 +0300

    laba7 task3 and 4 done

---
+++ laba7/task4/task4/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.чтениеИзФайлаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.записьВФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 40);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.чтениеИзФайлаToolStripMenuItem,
+            this.записьВФайлToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(91, 38);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // чтениеИзФайлаToolStripMenuItem
+            // 
+            this.чтениеИзФайлаToolStripMenuItem.Name = "чтениеИзФайлаToolStripMenuItem";
+            this.чтениеИзФайлаToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.чтениеИзФайлаToolStripMenuItem.Text = "Чтение из файла";
+            this.чтениеИзФайлаToolStripMenuItem.Click += new System.EventHandler(this.чтениеИзФайлаToolStripMenuItem_Click);
+            // 
+            // записьВФайлToolStripMenuItem
+            // 
+            this.записьВФайлToolStripMenuItem.Name = "записьВФайлToolStripMenuItem";
+            this.записьВФайлToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.записьВФайлToolStripMenuItem.Text = "Запись в файл";
+            this.записьВФайлToolStripMenuItem.Click += new System.EventHandler(this.записьВФайлToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(737, 354);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem чтениеИзФайлаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem записьВФайлToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
--- laba7/task4/task4/Form1.cs
+++ laba7/task4/task4/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
-namespace task3
+namespace task4
 {
     public partial class Form1 : Form
     {
@@ -18,19 +18,23 @@ namespace task3
             InitializeComponent();
         }
 
-        private void чтениеИзФайлаFileToolStripMenuItem_Click(object sender, EventArgs e)
+        private void чтениеИзФайлаToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
             {
-                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+                StreamReader reader = new StreamReader(openFileDialog1.FileName);
+                richTextBox1.Text = reader.ReadToEnd();
+                reader.Close();
             }
         }
 
-        private void записьВФайлFileToolStripMenuItem_Click(object sender, EventArgs e)
+        private void записьВФайлToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (saveFileDialog1.ShowDialog() != DialogResult.Cancel)
             {
-                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+                StreamWriter writer = new StreamWriter(saveFileDialog1.FileName, true);
+                writer.WriteLine(richTextBox1.Text);
+                writer.Close();
                 richTextBox1.Text = "";
             }
         }
