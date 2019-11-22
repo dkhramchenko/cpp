d61703ecc3f4366030d0a68c5137057d7bf38bb9
Qvocha <pineappletoe@gmail.com>
2019-11-22 17:02:22 +0300

    end

--- laba8/task2/task2/Form1.cs
+++ laba8/task2/task2/Form1.cs
@@ -16,5 +16,28 @@ namespace task2
         {
             InitializeComponent();
         }
+
+        // Динамическая коллецция для записи координат точек
+        private List<Point> Pts = new List<Point>();
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Получение объекта контекста устройства
+            Graphics g = this.CreateGraphics();
+            // Анализ нажатой кнопки мыши
+            if (e.Button == MouseButtons.Left)
+            {
+                // Если в коллекции уже 4 точки, то пора рисовать кривую Безье
+                if (Pts.Count == 4)
+                {
+                    g.DrawBezier(new Pen(Brushes.Red, 5), Pts[0], Pts[1], Pts[2], Pts[3]);
+                }
+                else
+                {
+                    // Если в коллекции ещё нет 4-х точек, то добавим очередную точку
+                    Pts.Add(new Point(e.X - 5, e.Y - 5, ))
+                }
+            }
+        }
     }
 }
--- laba8/task2/task2/Program.cs
+++ laba8/task2/task2/Program.cs
@@ -17,6 +17,8 @@ namespace task2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Form1 f = new Form1();
+            f.aaa = 90;
         }
     }
 }
