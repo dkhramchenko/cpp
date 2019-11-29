b2e33370b7a80406366c329e41a043a28d45266f
Qvocha <pineappletoe@gmail.com>
2019-11-29 15:11:12 +0300

    task2 done

---
+++ laba9/task2/task2/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task2
+{
+    public class Car
+    {
+        public string Brand;
+        public string Color;
+        public int Price;
+        public int Weight;
+    }
+
+    public struct Cars
+    {
+        public string Brand;
+        public string Color;
+        public int Price;
+        public int Weight;
+    }
+}
