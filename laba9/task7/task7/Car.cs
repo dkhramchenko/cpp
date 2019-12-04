510b58057686c85322bd95c0e0288a46cc2a35b5
Qvocha <pineappletoe@gmail.com>
2019-12-05 00:07:45 +0300

    laba9 task7 begin

---
+++ laba9/task7/task7/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    public class Car
+    {
+        private string brand;
+        public string Color;
+        private int price;
+        public int Weight;
+
+        public int Price { get => price; set => price = value; }
+        public string Brand 
+        {
+            get => brand;
+            set 
+            { 
+                if (Char.IsLower(value[0]))
+                {
+                    string s = value;
+                    s[0] = Char.ToUpper(s[0]);
+                    brand = s;
+                }
+            } 
+        }
+    }
+    public class Passenger : Car
+    {
+    }
+}
