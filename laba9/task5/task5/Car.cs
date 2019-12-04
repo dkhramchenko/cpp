d5b2d491355bc5fd6ab2e1f132ca5e8171627ac0
Qvocha <pineappletoe@gmail.com>
2019-12-04 15:01:38 +0300

    laba9 task5 done

---
+++ laba9/task5/task5/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    class Car
+    {
+        public static string Brand;
+        public const string Color = "Pink";
+        public int Price;
+        public int Weight;
+    }
+}
---
+++ laba9/task5/task5/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Car.Brand = "Audi";
+            Car c = new Car
+            {
+                Price = 100000,
+                Weight = 100000
+            };
+            richTextBox1.Text =
+                String.Format("марка: {0}; цвет: {1}; цена: {2}; вес: {3}",
+                Car.Brand, Car.Color, c.Price, c.Weight);
+            Car.Brand = "Audi";
+
+        }
+    }
+}
