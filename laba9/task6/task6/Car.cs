902030b81372eed50a01f18da72b61817222e408
Qvocha <pineappletoe@gmail.com>
2019-12-04 15:20:26 +0300

    laba9 task6 done

---
+++ laba9/task6/task6/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task6
+{
+    public class Car
+    {
+        public string Brand;
+        public string Color;
+        public int Weight;
+        private int price;
+
+        public int ProductionDate
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Price { get => price; set => price = value; }
+    }
+}
