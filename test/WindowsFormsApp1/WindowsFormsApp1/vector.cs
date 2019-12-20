c717c5e5b5d041dfadd1e8f191f0dcf178f07dc0
Qvocha <pineappletoe@gmail.com>
2019-12-20 18:36:56 +0300

    vvv

---
+++ test/WindowsFormsApp1/WindowsFormsApp1/vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Vector
+    {
+        public double[] data;
+        public double Length()
+        {
+            double r = 0;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                r += data[i] * data[i];
+            }
+            return System.Math.Sqrt(r);
+        }
+        public double this[int i]
+        {
+            get
+            {
+                return data[i];
+            }
+            set
+            {
+                data[i] = value;
+            }
+        }
+        string Text()
+        {
+            string r = "(";
+            for (int i = 0; i < data.Length; ++i)
+            {
+                r += data[i].ToString() + ";";
+            }
+            r += ")";
+            return r;
+        }
+    }
+}
